21dbc3b1374cccfbef6a973169251b2cb0950675
veca22 <57525487+veca22@users.noreply.github.com>
2021-02-23 18:11:02 +0100

    Added logger in tests

--- src/test/java/selenium/CourseTests.java
+++ src/test/java/selenium/CourseTests.java
@@ -3,17 +3,24 @@ package selenium;
 import model.CourseForm;
 import model.ReceiptPage;
 import org.junit.jupiter.api.Assertions;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 public class CourseTests extends FunctionalTest {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Test
     public void newCourse() {
         //Test method for adding a new course
         driver.get("http://localhost:3000/course");
+        log.info(() -> "Successfully enter the localhost:3000/course for adding a new course. time: " + LocalDateTime.now());
 
         //Button for opening course form
         WebElement buttonPlus = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/button"));
@@ -22,45 +29,53 @@ public class CourseTests extends FunctionalTest {
 
         CourseForm courseForm = new CourseForm(driver);
         Assertions.assertTrue(courseForm.isInitialized());
+        log.info(() -> "Course form is initialized! time: " +  LocalDateTime.now());
         //I found save button with xpath
         courseForm.setSaveButton("/html/body/div/div/main/div[2]/div[2]/form/div[4]/button[1]");
         courseForm.newCourseForm("Course1","23", "3");
 
         ReceiptPage newCourseForm = courseForm.submitSave();
         Assertions.assertTrue(newCourseForm.isInitialized());
+        log.info(() -> "Submit new course was successfully! time: " + LocalDateTime.now());
     }
 
     @Test
     public void editCourse() {
         //Test method for edit course
         driver.get("http://localhost:3000/course");
+        log.info(() -> "Successfully enter the localhost:3000/course for editing course. time: " + LocalDateTime.now());
         //Click on element in table
         WebElement clickInTable = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/div[1]/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]"));
         clickInTable.click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         CourseForm courseForm = new CourseForm(driver);
         Assertions.assertTrue(courseForm.isInitialized());
+        log.info(() -> "Course form is initialized! time: " +  LocalDateTime.now());
         //I found save button with xpath
         courseForm.setSaveButton("/html/body/div/div/main/div[2]/div[2]/form/div[4]/button[1]");
         courseForm.editCourseForm("Course2");
         ReceiptPage editCoursePage = courseForm.submitSave();
         Assertions.assertTrue(editCoursePage.isInitialized());
+        log.info(() -> "Editing course was successfully! time: " + LocalDateTime.now());
     }
 
     @Test
     public void removeCourse() {
         //Test method for removing course
         driver.get("http://localhost:3000/course");
+        log.info(() -> "Successfully enter the localhost:3000/course for removing course. time: " + LocalDateTime.now());
         //Click on element in table
         WebElement clickInTable = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/div[1]/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]"));
         clickInTable.click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         CourseForm courseForm = new CourseForm(driver);
         Assertions.assertTrue(courseForm.isInitialized());
+        log.info(() -> "Course form is initialized! time: " +  LocalDateTime.now());
         //I found remove button with xpath
         courseForm.setRemoveButton("/html/body/div/div/main/div[2]/div[2]/form/div[4]/button[2]");
         ReceiptPage removeCoursePage = courseForm.submitRemove();
         Assertions.assertTrue(removeCoursePage.isInitialized());
+        log.info(() -> "Removing course was successfully! time: " + LocalDateTime.now());
     }
 
 }
--- src/test/java/selenium/TeacherTests.java
+++ src/test/java/selenium/TeacherTests.java
@@ -2,18 +2,24 @@ package selenium;
 
 import model.*;
 import org.junit.jupiter.api.Assertions;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 public class TeacherTests extends FunctionalTest {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Test
     public void newTeacher() {
         //Test method for adding a new teacher
         driver.get("http://localhost:3000/teacher");
+        log.info(() -> "Successfully enter the localhost:3000/teacher for adding a new teacher. time: " + LocalDateTime.now());
 
         //Button for opening teacher form
         WebElement buttonPlus = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/button"));
@@ -22,11 +28,13 @@ public class TeacherTests extends FunctionalTest {
 
         TeacherForm teacherForm = new TeacherForm(driver);
         Assertions.assertTrue(teacherForm.isInitialized());
+        log.info(() -> "Teacher form is initialized! time: " +  LocalDateTime.now());
         //I found save button with xpath
         teacherForm.setSaveButton("/html/body/div/div/main/div[2]/div[2]/form/div[4]/button[1]");
         teacherForm.newTeacher("Mirko","Vukovic", "mirko@gmail.com");
 
         ReceiptPage newTeacherForm = teacherForm.submitSave();
+        log.info(() -> "Submit new teacher was successfully! time: " + LocalDateTime.now());
         Assertions.assertTrue(newTeacherForm.isInitialized());
     }
 
@@ -34,39 +42,46 @@ public class TeacherTests extends FunctionalTest {
     public void editTeacher() {
         //Test method for edit teacher
         driver.get("http://localhost:3000/teacher");
+        log.info(() -> "Successfully enter the localhost:3000/teacher for edit teacher. time: " +  LocalDateTime.now());
         //Click on element in table
         WebElement clickInTable = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/div[1]/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]"));
         clickInTable.click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         TeacherForm teacherForm = new TeacherForm(driver);
         Assertions.assertTrue(teacherForm.isInitialized());
+        log.info(() -> "Teacher form is initialized! time: " +  LocalDateTime.now());
         //I found save button with xpath
         teacherForm.setSaveButton("/html/body/div/div/main/div[2]/div[2]/form/div[5]/button[1]");
         teacherForm.editTeacherData("Marko","Markovic");
         ReceiptPage editTeacherPage = teacherForm.submitSave();
         Assertions.assertTrue(editTeacherPage.isInitialized());
+        log.info(() -> "Edit teacher was successfully! time: " + LocalDateTime.now());
     }
 
     @Test
     public void removeTeacher() {
         //Test method for removing teacher
         driver.get("http://localhost:3000/teacher");
+        log.info(() -> "Successfully enter the localhost:3000/teacher for remove teacher. time: " +  LocalDateTime.now());
         //Click on element in table
         WebElement clickInTable = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/div[1]/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]"));
         clickInTable.click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         TeacherForm teacherForm = new TeacherForm(driver);
         Assertions.assertTrue(teacherForm.isInitialized());
+        log.info(() -> "Teacher form is initialized! time: " +  LocalDateTime.now());
         //I found remove button with xpath
         teacherForm.setRemoveButton("/html/body/div/div/main/div[2]/div[2]/form/div[5]/button[2]");
         ReceiptPage removeTeacherPage = teacherForm.submitRemove();
         Assertions.assertTrue(removeTeacherPage.isInitialized());
+        log.info(() -> "Remove teacher was successfully! time: " + LocalDateTime.now());
     }
 
     @Test
     public void assignCourseTeacher() {
         //Test method for assigns teacher courses
         driver.get("http://localhost:3000/teacher");
+        log.info(() -> "Successfully enter the localhost:3000/teacher for assign teacher course. time: " +  LocalDateTime.now());
         //Click on element in table
         WebElement clickInTable = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/div[1]/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]"));
         clickInTable.click();
@@ -92,12 +107,14 @@ public class TeacherTests extends FunctionalTest {
         teacherCourseForm.setSaveButton("/html/body/div/div/main/div[2]/div[3]/div[1]/form/div[2]/button[1]");
         ReceiptPage assignTeacherCourses = teacherCourseForm.submitSave();
         Assertions.assertTrue(assignTeacherCourses.isInitialized());
+        log.info(() -> "Assign teacher course was successfully! time: " + LocalDateTime.now());
     }
 
     @Test
     public void deleteTeacherCourse() {
         //Test method for unassigne teacher course
         driver.get("http://localhost:3000/teacher");
+        log.info(() -> "Successfully enter the localhost:3000/teacher for unassign teacher course. time: " +  LocalDateTime.now());
         //Click on element in table
         WebElement clickInTable = driver.findElement(By.xpath("/html/body/div/div/main/div[2]/div[1]/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]"));
         clickInTable.click();
@@ -117,5 +134,6 @@ public class TeacherTests extends FunctionalTest {
         teacherCourseForm.setRemoveButton("/html/body/div/div/main/div[2]/div[3]/div[1]/form/div[2]/button[1]");
         ReceiptPage removeTeacerCoursePage = teacherCourseForm.submitRemove();
         Assertions.assertTrue(removeTeacerCoursePage.isInitialized());
+        log.info(() -> "Unassign teacher course was successfully! time: " + LocalDateTime.now());
     }
 }
