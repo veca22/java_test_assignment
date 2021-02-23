9654e913b2ffe7c3e19c4e813a7832545b55d0e5
veca22 <57525487+veca22@users.noreply.github.com>
2021-02-23 10:27:53 +0100

    update
    -Teacher service done all methods
    -Added update and delete method in TeacherController

--- src/main/java/com/alasdoo/developercourseassignment/DeveloperCourseAssignment.java
+++ src/main/java/com/alasdoo/developercourseassignment/DeveloperCourseAssignment.java
@@ -2,11 +2,12 @@ package com.alasdoo.developercourseassignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan(basePackages = "com.alasdoo")
 @EnableJpaRepositories("com.alasdoo")
@@ -17,7 +18,7 @@ public class DeveloperCourseAssignment {
 		SpringApplication.run(DeveloperCourseAssignment.class, args);
 	}
 
-	/*@Bean
+	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 			return new WebMvcConfigurerAdapter() {
 					@Override
@@ -25,5 +26,5 @@ public class DeveloperCourseAssignment {
 							registry.addMapping("/**");
 					}
 			};
-	}*/
+	}
 }
